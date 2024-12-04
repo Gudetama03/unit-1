9111ebba0477e14c706819bba7584ba0c527c7e0
Gudetama03 <salmanbibf@gmail.com>
2024-12-04 21:32:06 +0400

    add Game logic(pseudocode)

--- js/app.js
+++ js/app.js
@@ -18,9 +18,31 @@ Simple breakdown of how the game works:
 
 /* 
 Game logic(pseudocode):
-
-
-
+1. Setup game deck(playing cards in array)
+2. Setup liveDeck(cards remaining in play)
+3. Setup used cards(cards no longer in play)
+4. Create a score counter for player, and for dealer
+5. Player counter visible, while dealer counter only shows the score of the face up cards
+6. Create the play option buttons (hit and stand)
+7. Setup play area 
+    a.section for player cards
+    b.section for dealer cards
+8. Start game
+    a. Give two random cards from deck to player
+    b. Push the used cards into in usedCards array, and then push the remaining cards into the liveDeck
+    c. Take the value of cards and add them to player counter
+    d. Give two random cards to dealer, one face up and one face down, do same logic to cards.
+    e. Only take the value of face up cards to counter
+    f. Give player 2 options, hit or stand
+        a. If hit, add a random card from liveDeck to hand
+        b. Check if score>=21, if false then bust, otherwise add value to player counter, and allow to hit or stand again
+        c. If stand, reveal dealer face down card
+            a.If counter<17 must hit until reach 17+
+            b.If councter>=17, stand
+    g. check win, compare values of dealer and player
+        a. If dealer>player (no more than 21) dealer win
+        b. if player>dealer (no more than 21 ) player win
+        c. if player==dealer push (no one wins)
 
 
 */
