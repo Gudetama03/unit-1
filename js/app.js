e1179216d20013d22a7ea99cf40f971747c7c45c
Gudetama03 <salmanbibf@gmail.com>
2024-12-08 10:52:19 +0400

    add deck and some testing

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ Simple breakdown of how the game works:
 */
 
 
-
 /* 
 Game logic(pseudocode):
 1. Setup game deck(playing cards in array)
@@ -44,5 +43,71 @@ Game logic(pseudocode):
         b. if player>dealer (no more than 21 ) player win
         c. if player==dealer push (no one wins)
 
-
 */
+
+const deck = [  
+    // Hearts  
+    { id: 0, card: "Ace of Hearts", link: "./resources/cards/ace_of_hearts.png" },  
+    { id: 1, card: "2 of Hearts", link: "" },  
+    { id: 2, card: "3 of Hearts", link: "" },  
+    { id: 3, card: "4 of Hearts", link: "" },  
+    { id: 4, card: "5 of Hearts", link: "" },  
+    { id: 5, card: "6 of Hearts", link: "" },  
+    { id: 6, card: "7 of Hearts", link: "" },  
+    { id: 7, card: "8 of Hearts", link: "" },  
+    { id: 8, card: "9 of Hearts", link: "" },  
+    { id: 9, card: "10 of Hearts", link: "" },  
+    { id: 10, card: "Jack of Hearts", link: "" },  
+    { id: 11, card: "Queen of Hearts", link: "" },  
+    { id: 12, card: "King of Hearts", link: "" },  
+
+    // Diamonds  
+    { id: 13, card: "Ace of Diamonds", link: "" },  
+    { id: 14, card: "2 of Diamonds", link: "" },  
+    { id: 15, card: "3 of Diamonds", link: "" },  
+    { id: 16, card: "4 of Diamonds", link: "" },  
+    { id: 17, card: "5 of Diamonds", link: "" },  
+    { id: 18, card: "6 of Diamonds", link: "" },  
+    { id: 19, card: "7 of Diamonds", link: "" },  
+    { id: 20, card: "8 of Diamonds", link: "" },  
+    { id: 21, card: "9 of Diamonds", link: "" },  
+    { id: 22, card: "10 of Diamonds", link: "" },  
+    { id: 23, card: "Jack of Diamonds", link: "" },  
+    { id: 24, card: "Queen of Diamonds", link: "" },  
+    { id: 25, card: "King of Diamonds", link: "" },  
+
+    // Clubs  
+    { id: 26, card: "Ace of Clubs", link: "" },  
+    { id: 27, card: "2 of Clubs", link: "" },  
+    { id: 28, card: "3 of Clubs", link: "" },  
+    { id: 29, card: "4 of Clubs", link: "" },  
+    { id: 30, card: "5 of Clubs", link: "" },  
+    { id: 31, card: "6 of Clubs", link: "" },  
+    { id: 32, card: "7 of Clubs", link: "" },  
+    { id: 33, card: "8 of Clubs", link: "" },  
+    { id: 34, card: "9 of Clubs", link: "" },  
+    { id: 35, card: "10 of Clubs", link: "" },  
+    { id: 36, card: "Jack of Clubs", link: "" },  
+    { id: 37, card: "Queen of Clubs", link: "" },  
+    { id: 38, card: "King of Clubs", link: "" },  
+
+    // Spades  
+    { id: 39, card: "Ace of Spades", link: "" },  
+    { id: 40, card: "2 of Spades", link: "" },  
+    { id: 41, card: "3 of Spades", link: "" },  
+    { id: 42, card: "4 of Spades", link: "" },  
+    { id: 43, card: "5 of Spades", link: "" },  
+    { id: 44, card: "6 of Spades", link: "" },  
+    { id: 45, card: "7 of Spades", link: "" },  
+    { id: 46, card: "8 of Spades", link: "" },  
+    { id: 47, card: "9 of Spades", link: "" },  
+    { id: 48, card: "10 of Spades", link: "" },  
+    { id: 49, card: "Jack of Spades", link: "" },  
+    { id: 50, card: "Queen of Spades", link: "" },  
+    { id: 51, card: "King of Spades", link: "" }  
+]
+
+
+// Change image 
+const imageTest=document.getElementById("test")
+imageTest.src=deck[0].link
